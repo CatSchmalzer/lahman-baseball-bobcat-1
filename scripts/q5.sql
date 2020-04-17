7853c39f9007d0ac2fed39d75d9a8cd576d770c0
Cristina Attfield <51496177+cattfield@users.noreply.github.com>
2020-04-17 15:37:17 -0500

    Update q5.sql
    Updated code to correctly output averages PER GAME by DIVIDING by 2.
    
    i.e. hr/2 and so/2

--- scripts/q5.sql
+++ scripts/q5.sql
@@ -1,6 +1,8 @@
 --Question 5
 --Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
-select floor(yearid/10)*10 as decade, round(avg(hr),2) as avg_homerun, round(avg(so),2) as avg_strikeouts
+select floor(yearid/10)*10 as decade, 
+round(avg(hr/2),2) as avg_homerun_pergame, 
+round(avg(so/2),2) as avg_strikeouts_pergame
 from batting
 where yearid >= 1920
 group by decade;
